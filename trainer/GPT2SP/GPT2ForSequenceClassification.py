393504906c3e05ff9703d0e90eacf576a3a0c78a
Sansith <seniyadissanayake@gmail.com>
2024-02-02 02:01:56 +0530

    Update GPT2ForSequenceClassification model architecture - new layer

--- trainer/GPT2SP/GPT2ForSequenceClassification.py
+++ trainer/GPT2SP/GPT2ForSequenceClassification.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from transformers import GPT2Model, GPT2PreTrainedModel
 import torch
 from torch.nn import CrossEntropyLoss
+from torch.nn import functional as F
 
 
 class GPT2ForSequenceClassification(GPT2PreTrainedModel):
@@ -14,8 +15,12 @@ class GPT2ForSequenceClassification(GPT2PreTrainedModel):
         self.transformer = GPT2Model(config)
         self.dense1 = nn.Linear(config.n_embd, 4 * config.n_embd, bias=False)
         self.norm1 = nn.LayerNorm(4 * config.n_embd)
-        self.dense2 = nn.Linear(4 * config.n_embd, config.n_embd, bias=False)
-        self.norm2 = nn.LayerNorm(config.n_embd)
+
+        self.dense2 = nn.Linear(4 * config.n_embd, 2 * config.n_embd, bias=False)
+        self.norm2 = nn.LayerNorm(2 * config.n_embd)
+
+        self.dense3 = nn.Linear(2 * config.n_embd, config.n_embd, bias=False)
+        self.norm3 = nn.LayerNorm(config.n_embd)
 
         self.score = nn.Linear(config.n_embd, self.num_labels, bias=False)
 
@@ -71,6 +76,10 @@ class GPT2ForSequenceClassification(GPT2PreTrainedModel):
 
         hidden_states = self.dense2(hidden_states)
         hidden_states = self.norm2(hidden_states)
+        hidden_states = torch.relu(hidden_states)
+
+        hidden_states = self.dense3(hidden_states)
+        hidden_states = self.norm3(hidden_states)
         hidden_states = torch.relu(hidden_states)
 
         logits = self.score(hidden_states)
